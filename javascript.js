6477f086159be99fc83a6f67cb54c8511f2cb978
Lawliet00 <36177491+Lawliet00@users.noreply.github.com>
2018-03-14 15:05:53 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,155 +1,131 @@
 
-$(function(){
+// precio del costo de estampado y bordado por cm2
+var Precio_estampado_xCm2 = 0.05;
+var Precio_bordado_xCm2 = 0.08;
 
-  var Costo_total = [0,0,0,0,0,0]; //[sueter, h_derecho, h_izquierdo, pecho, espalda]
+// centimetros maximos permitidos para el bordado o estampado en la zona del hombro
+var Tam_max_hombro_Alto = 8;
+var Tam_max_hombro_Ancho = 8;
 
-  var Precio_estampado_xCm2 = 0.50;
-  var Precio_bordado_xCm2 = 0.70;
+// centimetros maximos permitidos para el bordado o estampado en la zona del frente y espalda
+var Tam_max_Alto = 35;
+var Tam_max_Ancho = 35;
 
-  var Tam_max_hombro_Alto = 8;
-  var Tam_max_hombro_Ancho = 7;
+// funcion encargada de obtener la informacion del formulario y mostrar los resultados
+function Cotizar(){
+  var Costo_total = 0;
 
-  var Tam_max_Alto = 35;
-  var Tam_max_Ancho = 30;
+  var camiseta = document.getElementById("camiseta").value;
+  if (valida_camiseta(camiseta) == false) {
+      alert("No elegio ninguna talla de camiseta");
+      return;
+  }
+  else{
+      Costo_total += valida_camiseta(camiseta);
+  }
 
-  $("#calcular_total").click(function(){
-    var total = 0;
-    for (var i = 0; i < Costo_total.length; i++) {
-      total += Costo_total[i];
-    }
-    console.log("precio total: "+ total);
-  });
-
-  $("#cotizar").click(function(){
-
-    for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < 4; i++) {
       var total = 0;
-      var acabado = document.getElementsByName("acabado"+String(i));
+      var acabado1 = document.getElementById("estampado"+String(i));
+      var acabado2 = document.getElementById("bordado"+String(i));
       var alto = document.getElementById("alto"+String(i)).value;
       var ancho = document.getElementById("ancho"+String(i)).value;
       var total_field = document.getElementById("total"+String(i));
-
-      if (acabado[0].checked) {
+    
+      if (acabado1.checked) {////
         total = Estampados(alto,ancho,i+1);
-        console.log(Estampados(alto,ancho,i+1));
       }
-      else if(acabado[1].checked){
+      else if(acabado2.checked){
         total = Bordados(alto,ancho,i+1);
-        console.log(Bordados(alto,ancho,i+1));
       }
-      Costo_total[i+1] = total;
-      total_field.value = total;
-      Costo_total[5] += total;
+      total_field.value = total.toFixed(2);
+      Costo_total += total;
     }
       var total_field = document.getElementById("total4");
-      total_field.value = Costo_total[5];
-  });
+      total_field.value = parseFloat(Costo_total).toFixed(2);  
+  
+};
 
 
-  function valida_medidas(alto,ancho,alto_M,ancho_M){
-    if ((alto >= 5 && alto <= alto_M) && (ancho >= 5 && ancho <= alto_M)) {
-      return true;
-    }
-    return false;
-  };
-
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////
-
-  function Estampados(alto,ancho,pos_zona){
-    if (pos_zona == 1) {
-        Costo_total[pos_zona] = Estampado_Hombros(alto,ancho,"h_derecho");
-    }
-    else if (pos_zona == 2) {
-        Costo_total[pos_zona] = Estampado_Hombros(alto,ancho,"h_izquierdo");
-    }
-    else if (pos_zona == 3) {
-        Costo_total[pos_zona] = Estampado_Maximo(alto,ancho,"pecho");
-    }
-    else if (pos_zona == 4) {
-        Costo_total[pos_zona] = Estampado_Maximo(alto,ancho,"espalda");
-    }
-    return Costo_total[pos_zona];
-  };
-
-  function Estampado_Hombros(alto,ancho, hombro){
-    if (valida_medidas(alto,ancho,Tam_max_hombro_Alto,Tam_max_hombro_Ancho)) {
-
-      if (hombro == "h_derecho") {
-        return (alto*Precio_estampado_xCm2*ancho);
-      }
-      else{
-        return (alto*Precio_estampado_xCm2*ancho);
-      }
-    }
-    alert("tamaño no valido para mangas");
+// funcion que obtiene el costo de la camiseta
+function valida_camiseta(camiseta){
+  if (camiseta[0] === "-"){
     return 0;
-  };
+  }
+  var inicio = camiseta.search("-");
+  var final = camiseta.search("$");
+  return (parseInt(camiseta.slice(inicio+2,final-1)));
+};
 
-  function Estampado_Maximo(alto, ancho, parte){
+// funcion que evalua si las medidas ingresadas por el usuario son validas
+// para una cierta zona
+function valida_medidas(alto,ancho,alto_M,ancho_M){
+  if ((alto >= 5 && alto <= alto_M) && (ancho >= 5 && ancho <= alto_M)) {
+    return true;
+  }
+  return false;
+};
 
-    if (valida_medidas(alto,ancho,Tam_max_Alto,Tam_max_Ancho)) {
-      if (parte == "pecho") {
-        console.log("pecho");
-        return (alto*Precio_estampado_xCm2*ancho);
-      }
-      else{
-        console.log("espalda");
-        return (alto*Precio_estampado_xCm2*ancho);
-      }
-    }
-    alert("tamaño no valido para pecho o espalda");
-    return 0;
-  };
+/////////////////////////////////////////////////////////
+/////////////////    ESTAMPADO     //////////////////////
+/////////////////////////////////////////////////////////
 
+// un menu en el cual se determina que zona eligio el usuario, en caso de ser Estampado
+// y decide que funcion llamar
+function Estampados(alto,ancho,pos_zona){
+  if (pos_zona == 1 || pos_zona == 2) {
+      return Estampado_Hombros(alto,ancho);
+  }
+  else if (pos_zona == 3 || pos_zona == 4) {
+      return Estampado_Maximo(alto,ancho);
+  }
+};
 
+// se evalua la zona que se eligio hombro(derecho o izquierdo) y se le calcula el precio
+function Estampado_Hombros(alto,ancho){
+  if (valida_medidas(alto,ancho,Tam_max_hombro_Alto,Tam_max_hombro_Ancho)) {
+      return (alto*Precio_estampado_xCm2*ancho);
+  }
+  return 0;
+};
 
+// se evalua la zona que se eligio(frente o espalda) y se le calcula el precio
+function Estampado_Maximo(alto, ancho){
 
-
+  if (valida_medidas(alto,ancho,Tam_max_Alto,Tam_max_Ancho)) {
+      return (alto*Precio_estampado_xCm2*ancho);
+  }
+  return 0;
+};
 
 
 ///////////////////////////////////////////////////////
+///////////////////      BORDADO   ////////////////////
 ///////////////////////////////////////////////////////
-///////////////////////////////////////////////////////
 
-  function Bordados(alto,ancho,pos_zona){
-    if (pos_zona == 1) {
-        Costo_total[pos_zona] = Bordado_Hombros(alto,ancho,"h_derecho");
-    }
-    else if (pos_zona == 2) {
-        Costo_total[pos_zona] = Bordado_Hombros(alto,ancho,"h_izquierdo");
-    }
-    else if (pos_zona == 3) {
-        Costo_total[pos_zona] = Bordado_Maximo(alto,ancho,"pecho");
-    }
-    else if (pos_zona == 4) {
-        Costo_total[pos_zona] = Bordado_Maximo(alto,ancho,"espalda");
-    }
-    return Costo_total[pos_zona];
-  };
+// un menu en el cual se determina que zona eligio el usuario, en caso de ser Bordado
+// y decide que funcion llamar
+function Bordados(alto,ancho,pos_zona){
+  if (pos_zona == 1 || pos_zona == 2) {
+    return Bordado_Hombros(alto,ancho);
+  }
+  else if (pos_zona == 3 || pos_zona == 4) {
+    return Bordado_Maximo(alto,ancho);
+  }
+};
 
-  function Bordado_Hombros(alto,ancho, hombro){
-    if (valida_medidas(alto,ancho,Tam_max_hombro_Alto,Tam_max_hombro_Ancho)) {
-      if (hombro == "h_derecho") {
-        return (alto*Precio_bordado_xCm2*ancho);
-      }
-      else{
-        return (alto*Precio_bordado_xCm2*ancho);
-      }
-    }
-    return 0;
-  };
+// se evalua la zona que se eligio hombro(derecho o izquierdo) y se le calcula el precio
+function Bordado_Hombros(alto,ancho){
+  if (valida_medidas(alto,ancho,Tam_max_hombro_Alto,Tam_max_hombro_Ancho)) {
+    return (alto*Precio_bordado_xCm2*ancho);
+  }
+  return 0;
+};
 
-  function Bordado_Maximo(alto, ancho, parte){
-    if (valida_medidas(alto,ancho,Tam_max_Alto,Tam_max_Ancho)) {
-      if (parte == "pecho") {
-        return (alto*Precio_bordado_xCm2*ancho);
-      }
-      else{
-        return (alto*Precio_bordado_xCm2*ancho);
-      }
-    }
-    return 0;
-  };
-});
+// se evalua la zona que se eligio(frente o espalda) y se le calcula el precio
+function Bordado_Maximo(alto, ancho){
+  if (valida_medidas(alto,ancho,Tam_max_Alto,Tam_max_Ancho)) {
+    return (alto*Precio_bordado_xCm2*ancho);
+  }
+  return 0;
+};
