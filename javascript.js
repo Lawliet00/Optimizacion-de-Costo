aa444884fff30e67d2ec744e00fd6bc39400c9fe
Lawliet00 <36177491+Lawliet00@users.noreply.github.com>
2018-03-18 15:13:29 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,40 +1,41 @@
 
 // precio del costo de estampado y bordado por cm2
-var Precio_estampado_xCm2 = 0.05;
-var Precio_bordado_xCm2 = 0.08;
+var Precio_estampado_xCm2 = 0;
+var Precio_bordado_xCm2 = 0;
 
 //tamaño minimo de estampado o bordado
-var Tam_min = 5;
+var Tam_min = 0;
 
 // centimetros maximos permitidos para el bordado o estampado en la zona del hombro
-var Tam_max_hombro_Alto = 8;
-var Tam_max_hombro_Ancho = 8;
+var Tam_max_hombro_Alto = 0;
+var Tam_max_hombro_Ancho = 0;
 
 // centimetros maximos permitidos para el bordado o estampado en la zona del frente y espalda
-var Tam_max_Alto = 35;
-var Tam_max_Ancho = 31;
+var Tam_max_Alto = 0;
+var Tam_max_Ancho = 0;
 
+function Valores_predetermindados(){
 
-function Carga_tam(){
+  var _POST = document.getElementsByName("POST");
+  // precio del costo de estampado y bordado por cm2
+  Precio_estampado_xCm2 = _POST[0].value;
+  Precio_bordado_xCm2 = _POST[1].value;
 
-  for (var i = 0; i < 4; i++) {
-	var placeAlto = document.getElementById("alto"+i);
-	var placeAncho = document.getElementById("ancho"+i);
+  //tamaño minimo de estampado o bordado
+  Tam_min = _POST[2].value;
 
-	if (i <= 1) {
-	  placeAlto.placeholder = "centimetros " + Tam_min + " - " +Tam_max_hombro_Alto;
-	  placeAncho.placeholder = "centimetros " + Tam_min + " - " +Tam_max_hombro_Ancho;		
-	}
-	else{
-	  placeAlto.placeholder = "centimetros " + Tam_min + " - " +Tam_max_Alto;
-	  placeAncho.placeholder = "centimetros " + Tam_min + " - " +Tam_max_Ancho;
-	}  
-  }
+  // centimetros maximos permitidos para el bordado o estampado en la zona del hombro
+  Tam_max_hombro_Alto = _POST[3].value;
+  Tam_max_hombro_Ancho = _POST[4].value;
 
+  // centimetros maximos permitidos para el bordado o estampado en la zona del frente y espalda
+  Tam_max_Alto = _POST[5].value;
+  Tam_max_Ancho = _POST[6].value;
 };
 
 // funcion encargada de obtener la informacion del formulario y mostrar los resultados
 function Cotizar(){
+
   var Costo_total = 0;
 
   var camiseta = document.getElementById("camiseta").value;
@@ -50,22 +51,23 @@ function Cotizar(){
       var total = 0;
       var acabado1 = document.getElementById("estampado"+String(i));
       var acabado2 = document.getElementById("bordado"+String(i));
-      var alto = document.getElementById("alto"+String(i)).value; 
+      var alto = document.getElementById("alto"+String(i)).value;
       var ancho = document.getElementById("ancho"+String(i)).value;
       var total_field = document.getElementById("total"+String(i));
     
-      if (acabado1.checked) {////
+      if (acabado1.checked) {
         total = Estampados(alto,ancho,i+1);
       }
       else if(acabado2.checked){
+
         total = Bordados(alto,ancho,i+1);
       }
       total_field.value = total.toFixed(2);
       Costo_total += total;
-    }
-      var total_field = document.getElementById("total4");
-      total_field.value = parseFloat(Costo_total).toFixed(2);  
-  
+  }
+  var total_field = document.getElementById("total4");
+  total_field.value = parseFloat(Costo_total).toFixed(2);  
+
 };
 
 
